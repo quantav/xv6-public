85e1c180b5de9237e74b316d9faf8bb268a4a0e0
quantav <vivekbiswas1991@gmail.com>
2026-02-28 19:45:20 +0530

    local conf update

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,10 @@
 ((c-mode
   (indent-tabs-mode . nil)
+  (tab-width . 4)
   (c-file-style . "bsd")
-  (c-basic-offset . 2)))
+  (c-basic-offset . 4))
+
+ (asm-mode
+  (indent-tabs-mode . nil)
+  (tab-width . 4)
+  (asm-comment-column . 40)))
